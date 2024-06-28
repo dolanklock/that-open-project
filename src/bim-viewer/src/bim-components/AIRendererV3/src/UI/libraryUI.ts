64016bae044cb59b2efa0add61cc94ae1e66ba67
Dolan Klock <dolank16@gmail.comm>
2024-06-28 09:34:24 -0400

    Chore: clean up

--- src/bim-viewer/src/bim-components/AIRendererV3/src/UI/libraryUI.ts
+++ src/bim-viewer/src/bim-components/AIRendererV3/src/UI/libraryUI.ts
@@ -96,14 +96,45 @@ export default (components: OBC.Components, galleryDb: Gallery) => {
     const modal = BUI.Component.create<HTMLDialogElement>(() => {
         return BUI.html `
             <dialog>
-                <bim-panel> 
-                    <div style="display: flex; flex-direction: column; gap: 1.5rem; padding: 20px;">
-                        <div style="display: flex; flex-direction: column; gap: .5rem;">
-                            <bim-label style="display: flex" icon="mingcute:rocket-fill">Height</bim-label>
-                            <bim-text-input value=""  vertical placeholder="Search..." debounce="200"></bim-text-input>
-                        </div>
-                        <div style="display: flex; flex-direction: row; gap: .5rem;">
-                            <bim-button @click=${() => modal.close()} label="Done" icon=""></bim-button>
+                <bim-panel>
+                    <div style="background-color: aqua; width: 1500px; height: 1500px; display: flex; justify-content: center; align-items: center;">
+                        <div id="carouselExampleCaptions" class="carousel slide">
+                            <div class="carousel-indicators">
+                            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                            </div>
+                            <div class="carousel-inner">
+                            <div class="carousel-item active">
+                                <img src="test.jpg" class="d-block w-100" alt="...">
+                                <div class="carousel-caption d-none d-md-block">
+                                <h5>First slide label</h5>
+                                <p>Some representative placeholder content for the first slide.</p>
+                                </div>
+                            </div>
+                            <div class="carousel-item">
+                                <img src="testthree.jpg" class="d-block w-100" alt="...">
+                                <div class="carousel-caption d-none d-md-block">
+                                <h5>Second slide label</h5>
+                                <p>Some representative placeholder content for the second slide.</p>
+                                </div>
+                            </div>
+                            <div class="carousel-item">
+                                <img src="test.png" class="d-block w-100" alt="...">
+                                <div class="carousel-caption d-none d-md-block">
+                                <h5>Third slide label</h5>
+                                <p>Some representative placeholder content for the third slide.</p>
+                                </div>
+                            </div>
+                            </div>
+                            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Previous</span>
+                            </button>
+                            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Next</span>
+                            </button>
                         </div>
                     </div>
                 </bim-panel>
