c23f3d141636a2f85fc190fcb03b5022eeebd609
Dolan Klock <dolank16@gmail.comm>
2024-07-05 15:57:57 -0400

    Feat: updates to ai renderer tool
    updates to UI

--- src/bim-viewer/src/bim-components/AIRendererV4/src/UI/src/Screenshots.ts
+++ src/bim-viewer/src/bim-components/AIRendererV4/src/UI/src/Screenshots.ts
@@ -5,7 +5,9 @@ import {Gallery} from "../../DataBase/RenderLibraryDB"
 import PromptUI from "../PromptUI"
 
 export default (components: OBC.Components, galleryDb: Gallery) => {
-    const containerHTMLElement = document.createElement("ol") as HTMLOListElement
+    const containerHTMLElement = document.createElement("ol") as HTMLElement
+    // containerHTMLElement.style.paddingRight = "10px"
+    containerHTMLElement.style.padding = "0px"
     // would need to iterate through db and get each image url and other data, etc
     // and render to UI
     for (let i = 0; i < 10; i++) {
